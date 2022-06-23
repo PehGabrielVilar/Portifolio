2bb295f3b71614133266a63a19be0d953b0a3f27
Pedro Gabriel Vilar <47572621+PehGabrielVilar@users.noreply.github.com>
2022-06-23 19:29:55 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ const imagem = [
   },
   {
     title: "Thor",
-    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Thor.jpg?raw=true",
+    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Thor.jpeg?raw=true",
     info: "Herói e membro dos vingadores, criado pela Marvel."
   },
   {
@@ -19,14 +19,14 @@ const imagem = [
     info: "Jogador de futebol eleito 5 vezes melhor do mundo."
   },
   {
-    title: "Doodles",
-    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Doodles.jpeg?raw=true",
-    info: "Personagens de várias animações diferentes, juntos."
+    title: "?",
+    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Livre.jpeg?raw=true",
+    info: "Estilo livre, criado por Pedro Gabriel Vilar"
   },
   {
-    title: "Arte Livre",
-    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Tanjiro.jpeg?raw=true",
-    info: "Criação minha."
+    title: "Seja seu Herói",
+    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Hero.jpeg?raw=true",
+    info: "Estilo livre, criado por Pedro Gabriel Vilar"
   },
   {
     title: "Demon Slayer",
@@ -39,9 +39,9 @@ const imagem = [
     info: "Personagem do Anime Death Note, criado por Takeshi Obata."
   },
   {
-    title: "Shikamaru",
-    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Shikamaru.jpeg?raw=true",
-    info: "Personagem do Anime Naruton, criado por Masashi Kishimoto."
+    title: "Picles Rick",
+    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/PicklesRick.jpeg?raw=true",
+    info: "Animação criada por Justin Roiland e Dan Harmon"
   },
   {
     title: "Rick and Morty",
