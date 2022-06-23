ee6a2ffde0ca1be450e312775fa7d9c91e2ce904
Pedro Gabriel Vilar <47572621+PehGabrielVilar@users.noreply.github.com>
2022-06-23 02:33:50 +0000

    Add files via upload
    Arrumando e Testando bugs

--- scripts.js
+++ scripts.js
@@ -5,62 +5,62 @@ const card = document.querySelector("div.card");
 const imagem = [
   {
     title: "Thanos",
-    thumb: src = "imagens/thanos.jpeg",
+    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Thanos.jpeg?raw=true",
     info: "Vilão do Vingadores, criado pela Marvel."
   },
   {
     title: "Thor",
-    thumb: src = "imagens/thor.jpg",
+    thumb: "imagens/thor.jpg",
     info: "Herói e membro dos vingadores, criado pela Marvel."
   },
   {
     title: "Cristiano Ronaldo",
-    thumb: src = "imagens/cristiano.jpeg",
+    thumb: "imagens/cristiano.jpeg",
     info: "Jogador de futebol eleito 5 vezes melhor do mundo."
   },
   {
     title: "Doodles",
-    thumb: src = "imagens/doodles.jpeg",
+    thumb: "imagens/doodles.jpeg",
     info: "Personagens de várias animações diferentes, juntos."
   },
   {
     title: "Arte Livre",
-    thumb: src = "imagens/desenhos/tanjiro.jpeg",
+    thumb: "imagens/desenhos/tanjiro.jpeg",
     info: "Criação minha."
   },
   {
     title: "Demon Slayer",
-    thumb: src = "imagens/demonslayer.jpeg",
+    thumb: "imagens/demonslayer.jpeg",
     info: "Anime e Mangá criado por Koyoharu Gotouge."
   },
   {
     title: "Ryuk",
-    thumb: src = "imagens/ryuk.jpeg",
+    thumb: "imagens/ryuk.jpeg",
     info: "Personagem do Anime Death Note, criado por Takeshi Obata."
   },
   {
     title: "Shikamaru",
-    thumb: src = "imagens/shikamaru.jpeg",
+    thumb: "imagens/shikamaru.jpeg",
     info: "Personagem do Anime Naruton, criado por Masashi Kishimoto."
   },
   {
     title: "Rick and Morty",
-    thumb: src = "imagens/rickandmorty.jpeg",
+    thumb: "imagens/rickandmorty.jpeg",
     info: "Animação criada por Justin Roiland e Dan Harmon"
   },
   {
     title: "Auto-retrato",
-    thumb: src = "imagens/eu.jpeg",
+    thumb: "imagens/eu.jpeg",
     info: "Me fiz, em uma versão inspirada na banda Gorillaz."
   },
   {
     title: "Kobe Bryant",
-    thumb: src = "imagens/kobebryant.jpeg",
+    thumb: "imagens/kobebryant.jpeg",
     info: "Desenho inspirado na animação 'The Boondocks.'"
   },
   {
     title: "Arte Livre",
-    thumb: src = "imagens/sketch.jpeg",
+    thumb: "imagens/sketch.jpeg",
     info: "Inspirado em sketchs de tatuagem."
   }
 ];
@@ -69,8 +69,7 @@ imagem.map(imagem => {
   const cardClone = card.cloneNode(true);
   cardClone.querySelector("img").src = imagem.thumb;
   cardClone.querySelector(".title").innerHTML = imagem.title;
-  cardClone.querySelector(".info > p.text--medium").innerHTML =
-    imagem.info;
+  cardClone.querySelector(".info > p.text--medium").innerHTML = imagem.info;
   sectionCards.appendChild(cardClone);
 });
 
