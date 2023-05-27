1fe1999a9694cb01f70904ce485f83db7d704635
Pedro Gabriel Vilar <47572621+PehGabrielVilar@users.noreply.github.com>
2023-05-27 16:30:33 -0300

    Resolvendo o bug do scroll da pagina

--- id.js
+++ id.js
@@ -2,22 +2,51 @@ const menuItems = document.querySelectorAll('.menu a[href^="#"]');
 
 menuItems.forEach(item => {
   item.addEventListener('click', scrollToIdOnClick);
-})
-
-function getScrollTopByHref(element) {
-  const id = element.getAttribute('href');
-  return document.querySelector(id).offsetTop;
-}
+});
 
 function scrollToIdOnClick(event) {
   event.preventDefault();
-  const to = getScrollTopByHref(event.target) - 0;
-  scrollToPosition(to);
+  const targetSection = document.querySelector(event.target.getAttribute('href'));
+  const targetPosition = targetSection.offsetTop;
+
+  scrollToPosition(targetPosition);
 }
 
 function scrollToPosition(to) {
-  window.scroll({
-    top: to,
-    behavior: "smooth",
-  });
+  const startPosition = window.pageYOffset;
+  const distance = to - startPosition;
+  const duration = 1000;
+  let start = null;
+
+  function scrollAnimation(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    const easing = easeInOutCubic(Math.min(progress / duration, 1));
+    const newPosition = startPosition + distance * easing;
+    window.scrollTo(0, newPosition);
+
+    if (progress < duration) {
+      requestAnimationFrame(scrollAnimation);
+    } else {
+      enableScroll();
+    }
+  }
+
+  function easeInOutCubic(t) {
+    return t < 0.5
+      ? 4 * t * t * t
+      : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+  }
+
+  disableScroll();
+  requestAnimationFrame(scrollAnimation);
 }
+
+function disableScroll() {
+  document.body.style.overflow = 'hidden';
+}
+
+function enableScroll() {
+  document.body.style.overflow = 'auto';
+}
+
