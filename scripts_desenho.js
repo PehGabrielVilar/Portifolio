c22a0741ecf558612720568ba164448a1847aab1
Pedro Gabriel Vilar <47572621+PehGabrielVilar@users.noreply.github.com>
2023-07-25 22:50:53 -0300

    Add files via upload

--- scripts_desenho.js
+++ scripts_desenho.js
@@ -1,36 +1,53 @@
 const sectionCards = document.querySelector("section.cards");
-
 const card = document.querySelector("div.card");
 
 const imagem = [
   {
-    title: "Seja seu Herói",
-    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Hero.jpg?raw=true",
-    info: "Desenho livre, criado por Pedro Gabriel Vilar."
+    id: "dropzart_/",
+    title: "Logotipo",
+    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Logotipo.jpg?raw=true",
+    info: "Logotipo do meu instagram"
   },
   {
-    title: "Tanjiro",
-    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Tanjiro.jpg?raw=true",
-    info: "Desenho do personagem do anime e mangá Demon Slayer"
-  },
-  {
-    title: "Autorretrato",
+    id: "dropzart_/",
+    title: "Programando",
     thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Eu.jpg?raw=true",
-    info: "Fiz um autorretrato inspirado em animações."
+    info: "Fiz um autorretrato, programando"
   },
   {
+    id: "p/Ct9KroOriGV5uQjncWpyuj00h54jclrJ2dQ4oI0/",
+    title: "Ouvindo Música",
+    thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/Eu2.jpg?raw=true",
+    info: "Fiz um autorretrato, inspirado em animações."
+  },
+  { 
+    id: "dropzart_/",
     title: "Kobe Bryant",
     thumb: "https://github.com/PehGabrielVilar/PehGabrielVilar.github.io/blob/master/imagens/desenhos/KobeBryant.jpeg?raw=true",
     info: "Desenho inspirado na animação 'The Boondocks'."
   },
 ];
 
-imagem.map(imagem => {
+imagem.map(imageData => {
   const cardClone = card.cloneNode(true);
-  cardClone.querySelector("img").src = imagem.thumb;
-  cardClone.querySelector(".title").innerHTML = imagem.title;
-  cardClone.querySelector(".info > p.text--medium").innerHTML = imagem.info;
+  cardClone.querySelector("img").src = imageData.thumb;
+  cardClone.querySelector(".title").innerHTML = imageData.title;
+  cardClone.querySelector(".info > p.text--medium").innerHTML = imageData.info;
+
   sectionCards.appendChild(cardClone);
+});
+
+sectionCards.addEventListener("click", event => {
+  const clickedCard = event.target.closest(".card");
+  if (clickedCard) {
+    const titleElement = clickedCard.querySelector(".title");
+    const title = titleElement.textContent.trim();
+    const image = imagem.find(imageData => imageData.title === title);
+    if (image) {
+      const githubLink = `https://www.instagram.com/${image.id}`;
+        window.open(githubLink, "_blank"); 
+    }
+  }
 });
 
 card.remove();
